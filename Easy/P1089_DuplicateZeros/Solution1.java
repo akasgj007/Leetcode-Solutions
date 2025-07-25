e5378ca70468c60a432bf8eced134a9b00af3d5c
Akash Babu GJ <36225867+akasgj007@users.noreply.github.com>
2025-07-25 16:32:30 +0530

    Create Solution1.java

---
+++ Easy/P1089_DuplicateZeros/Solution1.java
@@ -0,0 +1,19 @@
+class Solution1 {
+    public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+        int dup[] = new int[n];
+        int index = 0;
+        for (int i = 0; i < n && index < n; i++) {
+            if (arr[i] == 0) {
+                index += 2;
+            } else {
+                dup[index] = arr[i];
+                index++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = dup[i];
+        }
+    }
+}
