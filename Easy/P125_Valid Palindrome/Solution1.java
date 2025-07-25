8982e99e2f7a5370efe548960339bbfafc5f1308
Akash Babu GJ <36225867+akasgj007@users.noreply.github.com>
2025-07-25 16:53:29 +0530

    Create Solution1.java

---
+++ Easy/P125_Valid Palindrome/Solution1.java
@@ -0,0 +1,16 @@
+//Solution 1 : Using StringBuilder
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)) sb.append(Character.toLowerCase(ch));
+        }
+
+        String s1 = sb.toString();
+        String s2 = sb.reverse().toString();
+
+        return s1.equals(s2);
+    }
+}
