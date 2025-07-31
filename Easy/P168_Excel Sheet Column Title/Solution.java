237711378a293789b453d04b60f9120ef0518748
Akash Babu GJ <36225867+akasgj007@users.noreply.github.com>
2025-07-31 17:42:32 +0530

    Create Solution.java

---
+++ Easy/P168_Excel Sheet Column Title/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String convertToTitle(int n) {
+       StringBuilder sb = new StringBuilder();
+
+       while(n>0){
+            n--;
+            char c = (char) ((n%26)+'A');
+            sb.append(c);
+            n = n/26;
+       }
+
+       return sb.reverse().toString();
+    }
+}
