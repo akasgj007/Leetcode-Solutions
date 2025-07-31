51ceca19cd4551be442ca6be668b2884a5e1187f
Akash Babu GJ <36225867+akasgj007@users.noreply.github.com>
2025-07-31 17:32:24 +0530

    Update Solution.java

--- Easy/P58_Length of Last Word/Solution.java
+++ Easy/P58_Length of Last Word/Solution.java
@@ -1,19 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int n = s.length();
+        int n =-0;
         boolean started = false;
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        for(int i=n-1;i>=0;i--){
+
+        for(int i=s.length()-1;i>0;i--){
             char ch = s.charAt(i);
             if(ch != ' '){
                 started = true;
-                sb.append(ch);
-                count++;
+                n+=1;
             }
             else if(started) break;
         }
-        System.out.println(sb.reverse());
-        return count;
+        return n;
     }
 }
